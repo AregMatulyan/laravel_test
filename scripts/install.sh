37612b7e7f256b9657ce890b7d1525e9698c3102
areg.matulyan <areg.matulyan@learnship.com>
2021-03-15 11:04:22 +0400

    LR-5 App installation script correction

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,4 +8,4 @@ Starting installation of the Laravel Test App...
 cp .env.example .env
 composer install
 ./scripts/up.sh
-docker-compose exec laravel.test bash -c "php artisan migrate; ./scripts/ant.sh; php artisan storage:link; php artisan queue:work; php artisan key:generate"
+docker-compose exec laravel.test bash -c "php artisan migrate; ./scripts/ant.sh; php artisan storage:link; php artisan key:generate; php artisan queue:work"
